c49940d5feaae63425733626c929ecd8d7cb3df8
Grey Elerson <grey.l.elerson@gmail.com>
2021-02-08 14:34:49 -0700

    commit caesar algorithm

--- src/caesar.js
+++ src/caesar.js
@@ -4,10 +4,38 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
+  // this is the master index for coding and decoding
+  const alpha = "abcdefghijklmnopqrstuvwxyz".split("");
+  const codex = alpha.map((el, idx) => { return [el, idx] } );
 
   function caesar(input, shift, encode = true) {
-    // your solution code here
+    // short circuit in unfavorable conditions
+    if (shift == undefined || shift == 0 || shift > 25 || shift < -25) return false;
+
+    // do the same thing backward, when decoding
+    if (!encode) {shift *= -1;}
+
+    // split the input into individual chars after standardizing
+    input = input.toLowerCase().split("");
+
+    let result = input.map( (char, idx) => {
+      // is this a letter or a non-letter character?
+      if (alpha.indexOf(char) >= 0) {
+        // look up current letter index, then add the shift amount to it
+        let letterIndex = alpha.indexOf(char) + shift;
+        if (letterIndex > 25) {
+          letterIndex = (letterIndex % 25)-1;
+        }
+        if (letterIndex < 0) {
+          letterIndex += 26;
+        }
+        // return the letter matching the new shifted letter index
+        return codex.find(pair => pair[1] == letterIndex)[0]
+      }
+      return char;
+    })
+
+    return result.join('')
   }
 
   return {
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,66 @@
-// Write your tests here!
+let caesar = require('../src/caesar');
+let expect = require('chai').expect;
+
+const defaultInput = "Voila! In view-- a humble vaudvillian villain...";
+const differentCase = "voila! in view-- a HUMBLE vaudvillian viLLain...";
+const shiftText = "xyz";
+
+describe("#caesar()", () => {
+
+    it('should return false when `shift` is undefined', () => {
+        let actual = caesar(defaultInput,undefined,true);
+        expect(actual).to.be.false;
+    });
+
+    it('should return false when `shift` is 0', () => {
+        let actual = caesar(defaultInput,0,true);
+        expect(actual).to.be.false;
+    });
+    
+    it('should return false when `shift` is less than -25', () => {
+        let actual = caesar(defaultInput,-30,true);
+        expect(actual).to.be.false;
+    });
+    
+    it('should return false when `shift` is greater than 25', () => {
+        let actual = caesar(defaultInput,30,true);
+        expect(actual).to.be.false;
+    });
+    
+    it('should maintain positions of all spaces', () => {
+        let actual = caesar(defaultInput,1,true);
+        let expected = "wpjmb! jo wjfx-- b ivncmf wbvewjmmjbo wjmmbjo..."
+        expect(actual).to.eql(expected);
+    });
+    
+    it('should maintain positions of all non-word characters', () => {
+        let actual = caesar(defaultInput,1,true);
+        let expected = "wpjmb! jo wjfx-- b ivncmf wbvewjmmjbo wjmmbjo..."
+        expect(actual).to.eql(expected);
+    });
+    
+    it('should not be case-sensitive', () => {
+        let actual = caesar(differentCase,1,true);
+        let expected = "wpjmb! jo wjfx-- b ivncmf wbvewjmmjbo wjmmbjo..."
+        expect(actual).to.eql(expected);
+    });
+    
+    it('should cycle positions from the end to beginning of the alphabet', () => {
+        let actual = caesar(shiftText,3,true);
+        let expected = "abc";
+        expect(actual).to.eql(expected);
+    });
+
+    it('should decode text when `encode` parameter is false', () => {
+        let actual = caesar(shiftText,3,false);
+        let expected = "uvw";
+        expect(actual).to.eql(expected);
+    });
+
+    it('should decode text when `encode` parameter is false', () => {
+        let actual = caesar("abc",3,false);
+        let expected = "xyz";
+        expect(actual).to.eql(expected);
+    });
+
+})
