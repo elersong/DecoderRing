995b3843cd5345e5c8e597e71614663d480c7935
Grey Elerson <grey.l.elerson@gmail.com>
2021-02-08 14:36:23 -0700

    commit substitution algorithm

--- src/substitution.js
+++ src/substitution.js
@@ -4,10 +4,66 @@
 // of the anonymous function on line 6
 
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
+
+  // global reference alphabet
+  const alpha = "abcdefghijklmnopqrstuvwxyz".split('');
+  
+  // test if string contains any duplicate chars
+  function isUniq(str) {
+    let letters = {};
+    str.split('').forEach(char => {
+      if (letters[char]) {
+        letters[char] = false;
+      } else {
+        letters[char] = true;
+      }
+    });
+
+    return !Object.values(letters).includes(false);
+  }
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    // short circuit 
+    if (!alphabet || !isUniq(alphabet) || alphabet.length != 26) return false;
+
+    // create cypherKey codex
+    const codex = alphabet.split('');
+
+    // standardize the input and break it apart
+    let inputSplit = input.toLowerCase().split('');
+
+    let returnString = '';
+
+    if (encode) {
+      // hide the message
+
+      inputSplit.forEach(el => {
+        if (alpha.includes(el)) {
+          // lookup the cypher char and add it
+          returnString += codex[alpha.indexOf(el)];
+        } else {
+          // retain any non alphabet characters
+          returnString += el;
+        }
+      });
+
+      return returnString;
+
+    } else {
+      // begin decode
+      
+      inputSplit.forEach(el => {
+        if (codex.includes(el)) {
+          // lookup the cypher char and add it
+          returnString += alpha[codex.indexOf(el)];
+        } else {
+          // retain any non alphabet characters
+          returnString += el;
+        }
+      });
+
+      return returnString;
+    }
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,43 @@
-// Write your tests here!
+let substitution = require('../src/substitution');
+let expect = require('chai').expect;
+
+const defaultInput = "thinkful thinkful";
+const differentCase = "ThInKFuL thINKfUL";
+const cypherKey = "xoyqmcgrukswaflnthdjpzibev"
+
+describe("#substitution()", () => {
+    
+    it('should maintain positions of all spaces', () => {
+        let actual = substitution(defaultInput, cypherKey);
+        let expected = "jrufscpw jrufscpw";
+        expect(actual).to.eql(expected);
+    });
+
+    it('should output a string', () => {
+        let actual = substitution(defaultInput, cypherKey);
+        expect(actual).to.be.a("string");
+    });
+    
+    it('should not be case-sensitive', () => {
+        let actual = substitution(differentCase, cypherKey);
+        let expected = "jrufscpw jrufscpw";
+        expect(actual).to.eql(expected);
+    });
+
+    it('should return false if alphabet is not exactly 26 chars long', () => {
+        let actual = substitution(defaultInput, "abcde");
+        expect(actual).to.be.false;
+    });
+
+    it('should return false if each alphabet char isn\'t unique', () => {
+        let actual = substitution(defaultInput, "abcdefghijklmnoooostuvwxyz");
+        expect(actual).to.be.false;
+    });
+
+    it('should decode an input string when "encode" is false', () => {
+        let expected = "this is only a test";
+        let actual = substitution("jrud ud lfwe x jmdj", cypherKey, false);
+        expect(actual).to.eql(expected);
+    })
+
+})
